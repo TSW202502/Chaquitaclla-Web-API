95c5045bd3848ccc022f0b9e5638982ee5c2dc8d
fdaniel13s <u20221c628@upc.edu.pe>
2025-09-24 20:42:58 -0800

    feat: implement repository and unit of work patterns for crops management

---
+++ Chaquitaclla_API_TSW/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Chaquitaclla_API_TSW.Shared.Domain.Repositories;
+using Chaquitaclla_API_TSW.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace Chaquitaclla_API_TSW.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
